54d1415f61be2f38ced9f4e883922c02ef197a2a
pejovic <milutinpejovic@gmail.com>
2016-03-06 23:25:31 +0100

    initial

---
+++ R/penint3D.R
@@ -0,0 +1,68 @@
+## 3D Penalized regression with interactions 
+
+penint3D<-function(fun,regdat,contVar,flist,lambda=seq(0,5,0.1),int=TRUE,depth.fun=list("linear","nspline","poly"),df=4,deg=3,preProc=TRUE){
+  
+  tvar<-as.character(fun)[2]
+  covs<-gsub("[[:space:]]", "", as.character(fun)[3])
+  covs<-unlist(strsplit(covs, "+", fixed = TRUE))
+  depth<-covs[length(covs)]
+  covs<-covs[-length(covs)]
+  
+  
+  if(int==TRUE){
+    fun<-as.formula(paste(tvar, "~" ,paste(covs,depth,sep="*", collapse="+"),sep=""))
+  } else {fun<-fun}
+  
+  
+  
+  if(preProc==TRUE){
+    preProcValues <- preProcess(regdat[,contVar], method = c("center", "scale"))
+    Trans.df<-cbind(predict(preProcValues, regdat[,contVar]),regdat[,-which(names(regdat) %in% contVar)])
+  }
+  
+  modmat <- model.matrix(fun ,Trans.df)[,-1] #fm.int.lm.As, # fm.GSIF.int.lm.As
+  # removing nzv varaibles 
+  nzv <- nearZeroVar(modmat)
+  if(sum(nzv)!=0){modmat <- modmat[, -nzv]}else{modmat<-modmat}
+  
+  # removing correlated variables 
+  corr_mat <- cor(modmat)
+  too_high <- findCorrelation(corr_mat, cutoff = .9)
+  if(sum(too_high)!=0){modmat <- modmat[, -too_high]}else{modmat<-modmat}
+  
+  
+  allData<-cbind(regdat[,paste(tvar)],modmat,regdat[,c("ID","longitude","latitude")])
+  names(allData)<-gsub("\\(altitude,.df.=.4\\)","",names(allData))
+  
+  results<-data.frame(lambda=rep(NA,length(flist)),RMSE=rep(NA,length(flist)),Rsquared=rep(NA,length(flist)))
+  coef.list=as.list(rep(NA,length(strat)))
+  for(i in 1:length(flist)){
+    ind<-which(allData$ID %in% do.call(c,flist[-i]))
+    TrainData<-as.data.frame(do.call(cbind,allData[ind,]))
+    Train.ID.index<-flist[-i]
+    TestData<-as.data.frame(do.call(cbind,allData[ind,]))
+    Test.ID.Index<-flist[i]
+    
+    folds1<-length(Train.ID.index)
+    folds.list1<-as.list(rep(NA,folds1))
+    names(folds.list1)<-paste("fold",c(1:folds1),sep = "")
+    foldid<-rep(NA,dim(TrainData)[1])
+    for(j in 1:length(Train.ID.index)){
+      folds.list1[[j]]<-which(TrainData$ID %in% Train.ID.index[[j]])
+      foldid[folds.list1[[j]]]<-j
+    }
+    
+    TrainData<-TrainData[,1:(dim(TrainData)[2]-3)]
+    TestData<-TestData[,1:(dim(TestData)[2]-3)]
+    #lambdaGrid <- seq(0,2.5,0.05)#10^seq(10,-2, length =100)
+    lasso.mod=cv.glmnet(as.matrix(TrainData[,-1]),TrainData[,1],alpha=1,lambda=lambda,foldid=foldid,type.measure="mse")
+    lasso.pred<-predict(lasso.mod,s=lasso.mod$lambda.min,newx=as.matrix(TestData[,-1]))
+    obs.pred<-data.frame(obs=TestData[,1],pred=as.numeric(lasso.pred))
+    coef.list[[i]]<-predict(lasso.mod,type="coefficients",s=lasso.mod$lambda.min)
+    dfresults<-data.frame(lambda=lasso.mod$lambda.min,RMSE=defaultSummary(obs.pred)[1],Rsquared=defaultSummary(obs.pred)[2])
+    results[i,]<-dfresults
+  }
+  coef.mat<-do.call(cbind,coef.list)
+  out<-list(measure=results,coef=coef.mat)
+  return(out)
+}
---
+++ R/stratFold3D.R
@@ -0,0 +1,79 @@
+# Create stratified folds
+
+stratfold3d <- function(targetVar,regdat,folds=6,cent=3,preProc=TRUE,seed=666,dimensions=list("2D","3D"),IDs=TRUE,sum=FALSE,plot=FALSE){
+  
+  dimensions<-dimensions[[1]]
+  if(dimensions=="2D"){
+    
+    unique.df<-ddply(regdat,.(ID),here(summarize),target=mean(eval(parse(text=targetVar))),longitude=longitude[1],latitude=latitude[1])
+    km <- kmeans(cbind(unique.df$longitude,unique.df$latitude), centers = cent)
+    #plot(unique.df$longitude,unique.df$latitude, col = km$cluster, pch = 20)
+    unique.df$km<-as.factor(km$cluster)} else {
+      
+      unique.df<-ddply(regdat,.(ID),here(summarize),target=mean(eval(parse(text=targetVar))),longitude=longitude[1],latitude=latitude[1],altitude=min(altitude))
+      km <- kmeans(cbind(scale(unique.df$longitude),scale(unique.df$latitude),scale(unique.df$altitude)), centers = cent)
+      #plot(unique.df$longitude,unique.df$latitude, col = km$cluster, pch = 20)
+      unique.df$km<-as.factor(km$cluster)
+    }
+  
+  
+  k.list<-as.list(rep(NA,length(unique(unique.df$km))))
+  names(k.list)<-paste("k",c(1:length(k.list)),sep="")
+  
+  
+  ########## Creating list of profile indices of 6 folds per cluster region ###############
+  for(i in 1:length(k.list)){
+    set.seed(seed)
+    k.list[[i]]<-createFolds(unique.df[which(unique.df$km==levels(unique.df$km)[i]),"target"],k=folds)
+    for(j in 1:folds){
+      k.list[[i]][[j]]<-unique.df[which(unique.df$km==levels(unique.df$km)[i]),"ID"][k.list[[i]][[j]]]
+    }
+  }
+  ##########################################################################################
+  
+  ################### Creating list of profile indices per fold ############################
+  ID.list<-as.list(rep(NA,folds))
+  names(ID.list)<-paste("fold",c(1:folds),sep = "")
+  for(i in 1:folds){
+    ID.list[[i]]<-do.call(c,lapply(k.list,function(x) x[[i]]))
+    names(ID.list[[i]])<-NULL
+    #folds.list[[i]]<-as.character(folds.list[[i]])
+  }
+  
+  ###########################################################################################
+  
+  ##############  Creating list of 
+  folds.list<-as.list(rep(NA,folds))
+  names(folds.list)<-paste("fold",c(1:folds),sep = "")
+  for(i in 1:length(ID.list)){
+    folds.list[[i]]<-which(regdat$ID %in% ID.list[[i]])
+  }
+  
+  
+  pom<-data.frame()
+  for(i in 1:length(folds.list)){
+    allData1<-regdat[folds.list[[i]],]
+    allData1$fold<-paste("fold",i,sep="")
+    allData<-rbind(allData1,pom)
+    pom<-allData
+  }
+  allData$fold<-factor(allData$fold)
+  sum.list=list(by(allData$altitude,allData$fold,summary))
+  if(IDs==TRUE){index.list=(ID.list)}else{index.list=(folds.list)}
+  sum.list<-list(allData,index.list,sum.list,by(allData[,paste(targetVar)],allData$fold,summary))
+  names(sum.list)<-c("Data","folds","altitude summary",paste(targetVar,"summary", sep=" "))
+  
+  if(plot==TRUE){
+    
+    allData.unique<-ddply(allData,.(ID),here(summarize),target=mean(eval(parse(text=targetVar))),longitude=longitude[1],latitude=latitude[1],fold=fold[1])
+    q <- ggplot(allData.unique,aes(x = longitude, y = latitude))
+    r <- q +geom_point(aes(size = sqrt(target/pi)), pch = 21, show.legend = FALSE) + scale_size_continuous(range=c(1,10))
+    r <- r + facet_wrap(~ fold)
+    r<-r + aes(fill = fold)
+    plot(r)
+  }
+  
+  if(sum==TRUE){return(sum.list)}else{return(sum.list[[1]])}
+}
+
+
