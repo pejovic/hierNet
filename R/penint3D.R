66f5b68cdc1f9f8811774fef9e76236ad73ba335
pejovic <milutinpejovic@gmail.com>
2016-03-10 19:39:46 +0100

    Dodata je opcija za hier=TRUE

--- R/penint3D.R
+++ R/penint3D.R
@@ -2,7 +2,7 @@
 ## Ubaciti hier==TRUE parametar pa ponoviti petlju, razdvojiti ova dva pristupa.
 
 
-penint3D<-function(fun,regdat,contVar,flist,lambda=seq(0,5,0.1),int=TRUE,depth.fun=list("linear","nspline","poly"),df=4,deg=3,preProc=TRUE){
+penint3D<-function(fun,regdat,contVar,flist,hier=TRUE,lambda=seq(0,5,0.1),int=TRUE,depth.fun=list("linear","nspline","poly"),df=4,deg=3,preProc=TRUE){
   
   tvar<-as.character(fun)[2]
   covs<-gsub("[[:space:]]", "", as.character(fun)[3])
@@ -10,6 +10,7 @@ penint3D<-function(fun,regdat,contVar,flist,lambda=seq(0,5,0.1),int=TRUE,depth.f
   depth<-covs[length(covs)]
   covs<-covs[-length(covs)]
   
+  if(!hier){
   
   if(int==TRUE){
     fun<-as.formula(paste(tvar, "~" ,paste(covs,depth,sep="*", collapse="+"),sep=""))
@@ -18,11 +19,10 @@ penint3D<-function(fun,regdat,contVar,flist,lambda=seq(0,5,0.1),int=TRUE,depth.f
   
   
   if(preProc==TRUE){
-    preProcValues <- preProcess(regdat[,contVar], method = c("center", "scale"))
-    Trans.df<-cbind(predict(preProcValues, regdat[,contVar]),regdat[,-which(names(regdat) %in% contVar)])
+    regdat[,c("altitude",contVar)]<-apply(regdat[,c("altitude",contVar)],2,scale)
   }
   
-  modmat <- model.matrix(fun ,Trans.df)[,-1] #fm.int.lm.As, # fm.GSIF.int.lm.As
+  modmat <- model.matrix(fun ,regdat)[,-1] #fm.int.lm.As, # fm.GSIF.int.lm.As
   # removing nzv varaibles 
   nzv <- nearZeroVar(modmat)
   if(sum(nzv)!=0){modmat <- modmat[, -nzv]}else{modmat<-modmat}
@@ -70,4 +70,86 @@ penint3D<-function(fun,regdat,contVar,flist,lambda=seq(0,5,0.1),int=TRUE,depth.f
   coef.mat<-do.call(cbind,coef.list)
   out<-list(measure=results,coef=coef.mat)
   return(out)
+
+  }else{
+    
+    
+    # Ovo je implementirano u hierNet-u samo sto nisam siguran sta radi sa factorskim promenljivima!!!
+    regdat[,c("altitude",contVar)]<-apply(regdat[,c("altitude",contVar)],2,scale)
+    
+    
+    modmat <- model.matrix(fun ,regdat)[,-1] 
+    # removing nzv varaibles 
+    nzv <- nearZeroVar(modmat)
+    if(sum(nzv)!=0){modmat <- modmat[, -nzv]}else{modmat<-modmat}
+    head(modmat)
+    
+    X <- hierNet::compute.interactions.c(modmat,diagonal=FALSE)
+    head(X)
+    
+    "%ni%" <- Negate("%in%")
+    (columns.to.keep <- c(paste0(names(data.frame(modmat)), ":",names(data.frame(modmat))), X %>%
+                            colnames() %>%
+                            grep("altitude" %>% as.character(),., value = T)))
+    
+    X[,colnames(X) %ni% columns.to.keep ] <- 0
+    head(X)
+    
+    allData<-cbind(tvar=regdat[,paste(tvar)],modmat,regdat[,c("ID","longitude","latitude")],X)
+    #names(allData)<-gsub("\\(altitude,.df.=.4\\)","",names(allData))
+    head(allData)
+    
+    results<-data.frame(lambda=rep(NA,length(flist)+1),RMSE=rep(NA,length(flist)+1),Rsquared=rep(NA,length(flist)+1))
+    coef.list=as.list(rep(NA,length(flist)))
+    pred<-data.frame()
+    
+    for(i in 1:length(flist)){
+      ind<-which(allData$ID %in% do.call(c,flist[-i])) # izdvajaje indeksa instanci koje pribadaju foldovima za trening
+      TrainData<-as.data.frame(do.call(cbind,allData[ind,])) # izdvajanje trening podacima
+      Train.ID.index<-flist[-i] # Izdvajanje dela liste foldova sa  trening podacima
+      TestData<-as.data.frame(do.call(cbind,allData[-ind,])) #Izdvajanje test podataka
+      Test.ID.Index<-flist[i] # Izdvajanje dela liste foldova sa test podacima
+      
+      folds1<-length(Train.ID.index) # duzina liste foldova sa trening podacima (tj. koliko foldova ide u tu listu)
+      folds.list1<-as.list(rep(NA,folds1)) # kreiranje prazne liste iste duzine
+      names(folds.list1)<-paste("fold",c(1:folds1),sep = "") # Nazivanje svakog clana liste po foldu...
+      foldid<-rep(NA,dim(TrainData)[1]) # kreiranje atributa koji ce nositi informaciju kom foldu pripada podatak 
+      
+      for(j in 1:length(Train.ID.index)){
+        folds.list1[[j]]<-which(TrainData$ID %in% Train.ID.index[[j]])
+        foldid[folds.list1[[j]]]<-j
+      }
+      
+      trainx <- as.matrix(TrainData[,colnames(modmat)]) 
+      testx <- as.matrix(TestData[,colnames(modmat)])
+      trainzz <- as.matrix(TrainData[,colnames(X)])
+      testzz <- as.matrix(TestData[,colnames(X)])
+      trainy <- (TrainData[,"tvar"])
+      testy <- (TestData[,"tvar"])
+      
+      fit = hierNet.path(trainx,trainy, zz = trainzz,diagonal=FALSE,strong=TRUE,flmin=0.1,trace=0)
+      fitcv = hierNet.cv(fit, trainx, trainy, folds=folds.list1, trace=0)
+      fit.def <- hierNet(trainx,trainy, zz = trainzz,diagonal=FALSE,strong=TRUE,lam=fit$lamlist[which(fitcv$lamhat==fit$lamlist)])
+      fit.pred <- predict(fit.def,newx=testx,newzz = testzz)
+      
+      ie <- as.matrix(fit$th[,,which(fitcv$lamhat==fit$lamlist)][,length(colnames(modmat))])
+      me<-fit$bp[,which(fitcv$lamhat==fit$lamlist), drop = F] - fit$bn[,which(fitcv$lamhat==fit$lamlist), drop = F]
+      rbind(ie,me)
+      
+      obs.pred <- data.frame(obs=testy,pred=fit.pred)
+      coef.list[[i]]<-rbind(ie,me)
+      dfresults<-data.frame(lambda=fitcv$lamhat,RMSE=defaultSummary(obs.pred)[1],Rsquared=defaultSummary(obs.pred)[2])
+      results[i,]<-dfresults
+      pred<-rbind(pred,obs.pred)
+    }
+    results[length(flist)+1,]<-c(NA,RMSE=defaultSummary(pred)[1],Rsquared=defaultSummary(pred)[2])
+    coef.mat<-do.call(cbind,coef.list)
+    out<-list(measure=results,coef=coef.mat)
+    return(out)
+  }
+
 }
+
+#####################################################################################
+
+
