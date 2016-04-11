9d80f45c074269ebee81fdbac6a216db80f41ac4
pejovic <milutinpejovic@gmail.com>
2016-04-11 21:09:32 +0200

    comment

--- R/script.R
+++ R/script.R
@@ -16,6 +16,9 @@ library(hierNet)
 library(magrittr)
 library(doParallel)
 library(foreach)
+library(stargazer)
+
+#load("pHPrediction2732016.RData")
 
 gk_7 <- "+proj=tmerc +lat_0=0 +lon_0=21 +k=0.9999 +x_0=7500000 +y_0=0 +ellps=bessel +towgs84=574.027,170.175,401.545,4.88786,-0.66524,-13.24673,0.99999311067 +units=m"
 utm <- "+proj=utm +zone=34 +ellps=GRS80 +towgs84=0.26901,0.18246,0.06872,-0.01017,0.00893,-0.01172,0.99999996031 +units=m"
@@ -80,53 +83,62 @@ plotfolds(rdat.folds,targetvar="SOM")
 
 #==============================================================================================================
 
-rez<-penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = FALSE, int=FALSE, depth.fun="linear")
-rez$measure
-summary(rez$measure[1:5,])
+BaseLcv <- penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = FALSE, int=FALSE, depth.fun="linear")
+BaseLcv$measure
+summary(BaseLcv$measure[1:5,])
 
 
-rezint<-penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = FALSE, int=TRUE, depth.fun="linear")
-rezint$measure
-summary(rezint$measure[1:5,])
+IntLcv <- penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = FALSE, int=TRUE, depth.fun="linear")
+IntLcv$measure
+summary(IntLcv$measure[1:5,])
 
 
-rezinthier<-penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = TRUE, int=FALSE, depth.fun="linear")
-rezinthier$measure
-summary(rezinthier$measure[1:5,])
+IntHLcv <- penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = TRUE, int=FALSE, depth.fun="linear")
+IntHLcv$measure
+summary(IntHLcv$measure[1:5,])
 
 
 #============= Poly ========================
-rez.poly<-penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = FALSE, int=FALSE, depth.fun="poly")
-rez.poly$measure
-summary(rez.poly$measure[1:5,])
+BasePcv <- penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = FALSE, int=FALSE, depth.fun="poly")
+BasePcv$measure
+summary(BasePcv$measure[1:5,])
 
 
-rezint.poly<-penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = FALSE, int=TRUE, depth.fun="poly")
-rezint.poly$measure
-summary(rezint.poly$measure[1:5,])
+IntPcv <- penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = FALSE, int=TRUE, depth.fun="poly")
+IntPcv$measure
+summary(IntPcv$measure[1:5,])
 
 
-rezinthier.poly<-penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = TRUE, int=TRUE, depth.fun="poly")
-rezinthier.poly$measure
-summary(rezinthier.poly$measure[1:5,])
+IntPHcv <- penint3D(fun=fun, profs = bor.profs, seed=443, cogrids = gridmaps.sm2D, hier = TRUE, int=TRUE, depth.fun="poly")
+IntPHcv$measure
+summary(IntPHcv$measure[1:5,])
 
+#=================== Nested 5 fold cross-validation results =======================
+
+summary.n5cv<-rbind(BaseLcv = BaseLcv$measure[6,], BasePcv = BasePcv$measure[6,], IntLcv = IntLcv$measure[6,], IntPcv = IntPcv$measure[6,], IntHLcv = IntHLcv$measure[6,], IntHPcv = IntHPcv$measure[6,])
+summary.n5cv[,1] <- rownames(summary.n5cv)
+rownames(summary.n5cv)<-NULL
+names(summary.n5cv)<-c("Model","RMSE","R squared")
+stargazer(summary.n5cv,summary=FALSE,digits=2)
+
+latex.table.by(summary.n5cv, num.by.vars = 2)
 
 #=================== predint3D ====================================================
 
-FFL<-predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = FALSE, int=FALSE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="linear",cores=8)
-FFP<-predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = FALSE, int=FALSE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="poly",cores=8)
+BaseL <- predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = FALSE, int=FALSE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="linear",cores=8)
+BaseP <- predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = FALSE, int=FALSE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="poly",cores=8)
 
-TFL<-predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = FALSE, int=TRUE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="linear",cores=8)
-TFP<-predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = FALSE, int=TRUE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="poly",cores=8)
+IntL <- predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = FALSE, int=TRUE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="linear",cores=8)
+IntP <- predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = FALSE, int=TRUE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="poly",cores=8)
 
-TTL<-predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = TRUE, int=TRUE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="linear",cores=8)
-TTP<-predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = TRUE, int=TRUE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="poly",cores=8)
+IntHL <- predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = TRUE, int=TRUE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="linear",cores=8)
+IntHP <- predint3D(fun=fun, profs = bor.profs, cogrids = gridmaps.sm2D, pred=FALSE ,hier = TRUE, int=TRUE, depths=c(-0.1,-0.2,-0.3) ,depth.fun="poly",cores=8)
 
 #============================ Prediction Summary =============================================
 
-summary.cv<-list(FFL=FFL$summary$results,FFP=FFP$summary$results,TFL=TFL$summary$results,TFP=TFP$summary$results,TTL=TTL$summary$results,TTP=TTP$summary$results)
-summary.coef<-list(FFL=FFL$summary$coefficients,FFP=FFP$summary$coefficients,TFL=TFL$summary$coefficients,TFP=TFP$summary$coefficients,TTL=TTL$summary$coefficients,TTP=TTP$summary$coefficients)
-summary.pred<-list(FFL=FFL$summary$pred,FFP=FFP$summary$pred,TFL=TFL$summary$pred,TFP=TFP$summary$pred,TTL=TTL$summary$pred,TTP=TTP$summary$pred)
+summary.cv<-list(BaseL=BaseL$summary$results[6,],BaseP=BaseP$summary$results[6,],IntL=IntL$summary$results[6,],IntP=IntP$summary$results[6,],IntHL=IntHL$summary$results[6,],IntHP=IntHP$summary$results[6,])
+summary.coef<-list(BaseL=BaseL$summary$coefficients,BaseP=BaseP$summary$coefficients,IntL=IntL$summary$coefficients,IntP=IntP$summary$coefficients,IntHL=IntHL$summary$coefficients,IntHP=IntHP$summary$coefficients)
+summary.pred<-list(BaseL=BaseL$summary$pred,BaseP=BaseP$summary$pred,IntL=IntL$summary$pred,IntP=IntP$summary$pred,IntHL=IntHL$summary$pred,IntHP=IntHP$summary$pred)
 
 
 summary.cv
@@ -178,80 +190,61 @@ maxR<-max(maxValue(subset(pred.stack, subset=sort(grep("L", names(prediction), v
 
 color.pal <- colorRampPalette(c("dark red","orange","light Yellow"),space="rgb")
 
-plot(subset(pred.stack, subset=sort(grep("L", names(prediction), value=TRUE)) , col=color.pal, breaks=seq(minR,maxR,length.out=30)))
+plot(subset(pred.stack, subset=sort(grep("TF", names(prediction), value=TRUE)) , col=color.pal, breaks=seq(minR,maxR,length.out=30)))
 
 
-#=========================== Prediction 0.2 ===================================================
+#=========================== Profile plots ===================================================
+
+FFL.p <- BaseL$summary$pred
+FFP.p <- BaseP$summary$pred
+TFL.p <- IntL$summary$pred
+TFP.p <- IntP$summary$pred
+TTL.p <- IntHL$summary$pred
+TTP.p <- IntHP$summary$pred
+
+names(FFL.p)<-c("ID","x","y","hdepth","altitude","observed","predicted","residual")
+names(FFP.p)<-c("ID","x","y","hdepth","altitude","observed","predicted","residual")
+names(TFL.p)<-c("ID","x","y","hdepth","altitude","observed","predicted","residual")
+names(TFP.p)<-c("ID","x","y","hdepth","altitude","observed","predicted","residual")
+names(TTL.p)<-c("ID","x","y","hdepth","altitude","observed","predicted","residual")
+names(TTP.p)<-c("ID","x","y","hdepth","altitude","observed","predicted","residual")
+
+res.profs<-data.frame(ID=TFL.p$ID,FFL.res=FFL.p$residual,FFP.res=FFP.p$residual,TFL.res=TFL.p$residual,TFP.res=TFP.p$residual,TTL.res=TTL.p$residual,TTP.res=TTP.p$residual)
+
+borind<-ddply(bor,.(ID))
+
+ind<-which(res.profs$ID %in% borind[,"ID"])
+
+res.profs<-cbind(res.profs,borind[ind,c("Top","Bottom","Soil.Type","x","y","altitude")])
+
+res.list<-list(sites=ddply(res.profs,.(ID),summarize,y=y[1],x=x[1],Soil.Type=Soil.Type[1]),horizons=res.profs[,c("ID","Top","Bottom","FFL.res","FFP.res","TFL.res","TFP.res","TTL.res","TTP.res")])
+res.profs<-join(res.list$sites,res.list$horizons, type="inner")
+
+
+depths(res.profs) <- ID ~ Top + Bottom
+site(res.profs) <- ~x+y
+coordinates(res.profs)<-~x+y
+proj4string(res.profs)<- CRS("+proj=utm +zone=34 +ellps=GRS80 +towgs84=0.26901,0.18246,0.06872,-0.01017,0.00893,-0.01172,0.99999996031 +units=m")
+
+# again, this time along 1-cm slices, computing quantiles
+agg <- slab(res.profs, fm= ~ FFL.res + FFP.res + TFL.res + TFP.res + TTL.res + TTP.res,slab.structure=seq(0,80,5))
+
+## see ?slab for details on the default aggregate function
+head(agg)
+
+xyplot(top ~ p.q50 | variable, data=agg, ylab='Depth',
+       xlab='median bounded by 25th and 75th percentiles',
+       lower=agg$p.q25, upper=agg$p.q75, ylim=c(80,-2),
+       panel=panel.depth_function,
+       alpha=0.25, sync.colors=TRUE,
+       par.settings=list(superpose.line=list(col='RoyalBlue', lwd=2)),
+       prepanel=prepanel.depth_function,
+       cf=agg$contributing_fraction, cf.col='black', cf.interval=5, 
+       layout=c(6,1), strip=strip.custom(bg=grey(0.8)),
+       scales=list(x=list(tick.number=4, alternating=3, relation='free'))
+)
 
 
 
-prediction0.1<-FFL$prediction[[1]][,"pred"]
-names(prediction0.1)<-"FFL0.1"
-
-prediction0.1$FFP0.1<-FFP$prediction[[1]]$pred
-
-prediction0.1$TFL0.1<-TFL$prediction[[1]]$pred
-prediction0.1$TFP0.1<-TFP$prediction[[1]]$pred
-
-prediction0.1$TTL0.1<-TTL$prediction[[1]]$pred
-prediction0.1$TTP0.1<-TTP$prediction[[1]]$pred
-
-minv<-min(prediction0.1@data)
-maxv<-max(prediction0.1@data)
-
-
-pred.stack0.1<-stack(prediction0.1)
-
-plot(pred.stack0.1, col=rev( heat.colors( 10 ) ),breaks=seq(minv,maxv,length.out=10))
-
-
-
-save.image("pHPrediction2632016.RData")
-
-
-
-
-
-str(TFL)
-names(predint3D.As.g)
-
-summary(predint3D.As.g$prediction[[1]]$pred)
-
-(predint3D.As.g$summary$results)
-
-
-pp<-raster(new3D[[2]],"pred")
-
-plot(pp)
-
-spplot(pp,"pred")
-
-
-load("Results2432016.RData")
-
-
-
-
-#================== Visualization ===================================
-p = get("cellsize", envir = GSIF.opts)[2]
-s = c(-.1,-.3)#get("stdepths", envir = GSIF.opts)
-
-sd.ll <- sapply(1:length(predint3D.pred), FUN=function(x){make.3Dgrid(predint3D.pred[[x]][c("pred")], pixelsize=p, stdepths=s[x])})
-
-sd.bb<-raster(sd.ll[[1]])
-plot(sd.bb)
-
-spplot(sd.ll[[1]],"pred")
-
-z0 = mean(gridmaps.sm2D$DEM, na.rm=TRUE)
-
-for(j in 1:length(sd.ll)){
-  kml(slot(SNDMHT.gsm, paste("sd", j, sep="")),
-              folder.name = paste("eberg_sd", j, sep=""),
-              file = paste("SNDMHT_sd", j, ".kml", sep=""),
-              colour = M, z.lim=c(10,85),
-              raster_name = paste("SNDMHT_sd", j, ".png", sep=""),
-              altitude = z0+5000+(s[j]*2500))
-   }
 
 
